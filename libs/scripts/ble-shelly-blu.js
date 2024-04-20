598c9ea8ac67c2ece405d39e4c8f280c24330296
kleieg <egbert.kleinhans@gmail.com>
2024-04-20 12:12:48 +0200

    auch für Xiaomi packedstrucct benutzen

--- libs/scripts/ble-shelly-blu.js
+++ libs/scripts/ble-shelly-blu.js
@@ -73,70 +73,6 @@ let packedStruct = {
   }
 };
 
-let mypackedStruct = {
-  buffer: '',
-  setBuffer: function(buffer) {
-    this.buffer = buffer;
-  },
-  utoi: function(u16) {
-    return (u16 & 0x8000) ? u16 - 0x10000 : u16;
-  },
-  getUInt8: function() {
-    return this.buffer.at(0)
-  },
-  getInt8: function() {
-    let int = this.getUInt8();
-    if(int & 0x80) int = int - 0x100;
-    return int;
-  },
-  getUInt16LE: function() {
-    return 0xffff & (this.buffer.at(1) << 8 | this.buffer.at(0));
-  },
-  getInt16LE: function() {
-    return this.utoi(this.getUInt16LE());
-  },
-  getUInt16BE: function() {
-    return 0xffff & (this.buffer.at(0) << 8 | this.buffer.at(1));
-  },
-  getInt16BE: function() {
-    return this.utoi(this.getUInt16BE(this.buffer));
-  },
-  unpack: function(fmt, keyArr) {
-    let b = '<>!';
-    let le = fmt[0] === '<';
-    if(b.indexOf(fmt[0]) >= 0) {
-      fmt = fmt.slice(1);
-    }
-    let pos = 0;
-    let jmp;
-    let bufFn;
-    let res = {};
-    
-    while(pos<fmt.length && pos<keyArr.length && this.buffer.length > 0) {
-
-      jmp = 0;
-      bufFn = null;
-      if(fmt[pos] === 'b' || fmt[pos] === 'B') jmp = 1;
-      if(fmt[pos] === 'h' || fmt[pos] === 'H') jmp = 2;
-      if(fmt[pos] === 'b') {
-        res[keyArr[pos]] = this.getInt8();
-      }
-      else if(fmt[pos] === 'B') {
-        res[keyArr[pos]] = this.getUInt8();
-      }
-      else if(fmt[pos] === 'h') {
-        res[keyArr[pos]] = le ? this.getInt16LE() : this.getInt16BE();
-      }
-      else if(fmt[pos] === 'H') {
-        res[keyArr[pos]] = le ? this.getUInt16LE() : this.getUInt16BE();
-      }
-      this.buffer = this.buffer.slice(jmp);
-      pos++;
-    }
-    return res;
-  }
-};
-
 
 let RuuviParser = {
   getData: function (res) {
@@ -146,9 +82,7 @@ let RuuviParser = {
       //print("xiaomi found",res.addr);
       //print("Bytes of data",res.advData.length);
 
-      mypackedStruct.setBuffer(res.advData);
-    
-      //print("xiaomi buffer copied");
+      packedStruct.setBuffer(res.advData);
       
       let rm = mypackedStruct.unpack('<BBBBBBBBBBBBBhHH', [
         'b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13',
@@ -157,50 +91,47 @@ let RuuviParser = {
         'batt'
       ]);
 
-  
-      //print("temp = ",rm.temp);
-      //print("hum = ",rm.hum);
-      //print("bat = ",rm.bat);
       rm.temp = rm.temp * 0.01; 
       rm.humidity = rm.humidity * 0.01;
       rm.batt = rm.batt * 0.001,
       rm.addr = res.addr.slice(0, -2);
       rm.rssi = res.rssi;
       return rm;
-    };  
+    }
+    else  {
+      let data = BLE.GAP.ParseManufacturerData(res.advData);
 
-    let data = BLE.GAP.ParseManufacturerData(res.advData);
-
-    if (typeof data !== "string" ||  data.length < 26) return null;
-    packedStruct.setBuffer(data);
-    let hdr = packedStruct.unpack('<HB', ['mfd_id', 'data_fmt']);
-    if(hdr.mfd_id !== RUUVI_MFD_ID) return null;
-    if(hdr.data_fmt !== RUUVI_DATA_FMT) {
-      print("unsupported data format from", res.addr);
-      print("expected format", RUUVI_DATA_FMT);
-      return null;
-    };
-    let rm = packedStruct.unpack('>hHHhhhHBHBBBBBB', [
-      'temp',
-      'humidity',
-      'pressure',
-      'acc_x',
-      'acc_y',
-      'acc_z',
-      'pwr',
-      'cnt',
-      'sequence',
-      'mac_0','mac_1','mac_2','mac_3','mac_4','mac_5'
-    ]);
-    rm.temp = rm.temp * 0.005;
-    rm.humidity = rm.humidity * 0.0025;
-    rm.pressure = rm.pressure + 50000;
-    rm.batt = ((rm.pwr >> 5) + 1600) / 1000;
-    rm.tx = (rm.pwr & 0x001f * 2) - 40;
-    rm.addr = res.addr.slice(0, -2);
-    rm.rssi = res.rssi;
-    return rm;
-  },
+      if (typeof data !== "string" ||  data.length < 26) return null;
+      packedStruct.setBuffer(data);
+      let hdr = packedStruct.unpack('<HB', ['mfd_id', 'data_fmt']);
+      if(hdr.mfd_id !== RUUVI_MFD_ID) return null;
+      if(hdr.data_fmt !== RUUVI_DATA_FMT) {
+        print("unsupported data format from", res.addr);
+        print("expected format", RUUVI_DATA_FMT);
+        return null;
+      };
+      let rm = packedStruct.unpack('>hHHhhhHBHBBBBBB', [
+        'temp',
+        'humidity',
+        'pressure',
+        'acc_x',
+        'acc_y',
+        'acc_z',
+        'pwr',
+        'cnt',
+        'sequence',
+        'mac_0','mac_1','mac_2','mac_3','mac_4','mac_5'
+      ]);
+      rm.temp = rm.temp * 0.005;
+      rm.humidity = rm.humidity * 0.0025;
+      rm.pressure = rm.pressure + 50000;
+      rm.batt = ((rm.pwr >> 5) + 1600) / 1000;
+      rm.tx = (rm.pwr & 0x001f * 2) - 40;
+      rm.addr = res.addr.slice(0, -2);
+      rm.rssi = res.rssi;
+      return rm;
+    }
+  }
 };
 
 function publishToMqtt(measurement) {
